de0c4165c62da37cb6e11e449c510517afbf41c8
Gogs <gogs@fake.local>
2021-09-24 09:42:57 +0900

    Initial commit

---
+++ app/src/main/java/com/example/list/Argentina.java
@@ -0,0 +1,16 @@
+package com.example.list;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+
+public class Argentina extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.argentina);
+    }
+}
